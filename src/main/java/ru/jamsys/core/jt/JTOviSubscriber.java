9a16351825975ca34b7b6d6ec6c3bb9583ac1328
urasfinks <urasfinks@yandex.ru>
2024-12-25 07:58:49 +0300

    + JTOviSubscriber

---
+++ src/main/java/ru/jamsys/core/jt/JTOviSubscriber.java
@@ -0,0 +1,37 @@
+package ru.jamsys.core.jt;
+
+import ru.jamsys.core.flat.template.jdbc.JdbcRequestRepository;
+import ru.jamsys.core.flat.template.jdbc.JdbcTemplate;
+import ru.jamsys.core.flat.template.jdbc.StatementType;
+
+public enum JTOviSubscriber implements JdbcRequestRepository {
+
+    SELECT("""
+            SELECT
+                *
+            FROM ovi_subscriber
+            WHERE
+                id_chat = ${IN.id_chat::NUMBER}
+            """, StatementType.SELECT_WITH_AUTO_COMMIT),
+
+    INSERT("""
+            INSERT INTO ovi_subscriber (
+                id_chat
+            )
+            VALUES (
+                ${IN.id_chat::NUMBER}
+            )
+            """, StatementType.SELECT_WITH_AUTO_COMMIT);
+
+    private final JdbcTemplate jdbcTemplate;
+
+    JTOviSubscriber(String sql, StatementType statementType) {
+        jdbcTemplate = new JdbcTemplate(sql, statementType);
+    }
+
+    @Override
+    public JdbcTemplate getJdbcTemplate() {
+        return jdbcTemplate;
+    }
+
+}
