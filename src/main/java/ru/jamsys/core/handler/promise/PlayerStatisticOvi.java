36fcef84c2a3fa963553170237b27b34bee358ed
urasfinks <urasfinks@yandex.ru>
2024-12-30 11:00:42 +0300

    Миграция на UtilNHL

--- src/main/java/ru/jamsys/core/handler/promise/PlayerStatisticOvi.java
+++ src/main/java/ru/jamsys/core/handler/promise/PlayerStatisticOvi.java
@@ -2,21 +2,14 @@ package ru.jamsys.core.handler.promise;
 
 import lombok.Getter;
 import lombok.Setter;
-import ru.jamsys.tank.data.NHLPlayerList;
+import ru.jamsys.core.flat.util.UtilNHL;
 
 @Getter
 @Setter
 public class PlayerStatisticOvi extends PlayerStatistic {
 
     public PlayerStatisticOvi() {
-        super(new NHLPlayerList.Player()
-                        .setPlayerID("3101")
-                        .setPos("LW")
-                        .setLongName("Alex Ovechkin")
-                        .setTeam("WSH")
-                        .setTeamID("31"),
-                853
-        );
+        super(UtilNHL.getOvi(), UtilNHL.getOviScoreLastSeason());
     }
 
 }
