5ec1050330bfe861bfeb8be4e02f4986e1c6b025
urasfinks <urasfinks@yandex.ru>
2024-12-30 11:03:18 +0300

    Добавлен каркас обработчика событий для бота ovi

---
+++ src/main/java/ru/jamsys/core/handler/promise/SendNotificationGameEventOvi.java
@@ -0,0 +1,61 @@
+package ru.jamsys.core.handler.promise;
+
+import lombok.Getter;
+import lombok.Setter;
+import ru.jamsys.core.App;
+import ru.jamsys.core.component.ServicePromise;
+import ru.jamsys.core.component.TelegramBotComponent;
+import ru.jamsys.core.flat.util.UtilNHL;
+import ru.jamsys.core.promise.Promise;
+import ru.jamsys.core.promise.PromiseGenerator;
+import ru.jamsys.tank.data.NHLPlayerList;
+import ru.jamsys.telegram.GameEventData;
+import ru.jamsys.telegram.template.GameEventTemplateOvi;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+public class SendNotificationGameEventOvi implements PromiseGenerator {
+
+    private final String idGame;
+
+    private final NHLPlayerList.Player player = UtilNHL.getOvi();
+
+    private final GameEventData gameEventData;
+
+    private final List<Integer> listIdChat = new ArrayList<>();
+
+    public SendNotificationGameEventOvi(
+            String idGame,
+            GameEventData gameEventData
+    ) {
+        this.idGame = idGame;
+        this.gameEventData = gameEventData;
+    }
+
+    @Override
+    public Promise generate() {
+        return App.get(ServicePromise.class).get(getClass().getSimpleName(), 60_000L)
+                .then("lastGoals", new ScorePlayerCurrentSeasonBeforeGame(player, idGame).generate())
+                .then("send", (atomicBoolean, _, promise) -> {
+                    String prevGoal = promise.getRepositoryMapClass(Promise.class, "lastGoals").getRepositoryMap(String.class, "prev_goal", "0");
+                    gameEventData
+                            .setPlayerName(NHLPlayerList.getPlayerName(player))
+                            .setGameName(idGame.substring(idGame.indexOf("_") + 1))
+                            .setScoredPrevGoal(Integer.parseInt(prevGoal));
+                    String message = new GameEventTemplateOvi(gameEventData).toString();
+                    TelegramBotComponent telegramBotComponent = App.get(TelegramBotComponent.class);
+                    System.out.println("SEND TO CLIENT: " + message);
+//                    UtilRisc.forEach(atomicBoolean, listIdChat, idChat -> {
+//                        if (telegramBotComponent.getNhlStatisticsBot() != null) {
+//                            telegramBotComponent.getNhlStatisticsBot().send(idChat, message, null);
+//                        }
+//                    });
+                })
+                .setDebug(false)
+                ;
+    }
+
+}
---
+++ src/main/java/ru/jamsys/telegram/template/GameEventTemplateOvi.java
@@ -0,0 +1,75 @@
+package ru.jamsys.telegram.template;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Getter;
+import lombok.Setter;
+import ru.jamsys.core.flat.template.twix.TemplateTwix;
+import ru.jamsys.core.flat.util.UtilNHL;
+import ru.jamsys.telegram.GameEventData;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+@Getter
+@Setter
+public class GameEventTemplateOvi {
+
+
+    final GameEventData data;
+
+    private String extra = "";
+
+    private String action = "";
+
+    private String scoredTitle = "";
+
+    private int goalsInSeason;
+
+    private int goalsInCareer;
+
+    private int gretzkyOffset;
+
+    public String template = "${action}! Game ${gameName}. ${playerName} scored ${scoredGoal} ${scoredTitle}${extra}. He has ${goalsInSeason} goals in season, ${goalsInCareer} goals in career and only ${gretzkyOffset} goals till Gretzky all-time record";
+
+    public GameEventTemplateOvi(GameEventData data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        if (!data.getScoredEnum().isEmpty()) {
+            extra = ": " + String.join(", ", data.getScoredEnum());
+        }
+        goalsInSeason = data.getScoredPrevGoal() + data.getScoredGoal();
+        goalsInCareer = goalsInSeason + data.getScoredLastSeason();
+        gretzkyOffset = UtilNHL.getScoreGretzky() - (goalsInCareer);
+        scoredTitle = data.getScoredGoal() > 1 ? "goals" : "goal";
+        action = data.getAction().toString();
+        if (action.equals("CANCEL_CORRECTION")) {
+            action = "CANCEL+CORRECTION";
+        }
+
+        Map<String, String> arg = new LinkedHashMap<>();
+        extend(arg, data);
+        extend(arg, this);
+        return TemplateTwix.template(template, arg, true);
+    }
+
+    private void extend(Map<String, String> arg, Object object) {
+        ObjectMapper om = new ObjectMapper();
+        om.convertValue(object, new TypeReference<Map<String, Object>>() {
+        }).forEach((key, value) -> {
+            if (value != null) {
+                if (value instanceof List<?>) {
+                    @SuppressWarnings("unchecked") List<String> value1 = (List<String>) value;
+                    arg.put(key, String.join(", ", value1));
+                } else {
+                    arg.put(key, value.toString());
+                }
+            }
+        });
+    }
+
+}
