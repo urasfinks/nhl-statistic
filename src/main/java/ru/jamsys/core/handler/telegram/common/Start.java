fa271a8d26608a50118baeeddebc653c15a4a3bf
urasfinks <urasfinks@yandex.ru>
2025-01-01 16:37:38 +0300

    + /start для NhlStatisticsBot

---
+++ src/main/java/ru/jamsys/core/handler/telegram/common/Start.java
@@ -0,0 +1,41 @@
+package ru.jamsys.core.handler.telegram.common;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.jamsys.core.component.ServicePromise;
+import ru.jamsys.core.promise.Promise;
+import ru.jamsys.core.promise.PromiseGenerator;
+import ru.jamsys.telegram.TelegramCommandContext;
+import ru.jamsys.telegram.handler.NhlStatisticsBotCommandHandler;
+
+@SuppressWarnings("unused")
+@Setter
+@Getter
+@Component
+@RequestMapping({"/start"})
+public class Start implements PromiseGenerator, NhlStatisticsBotCommandHandler {
+
+    private final ServicePromise servicePromise;
+
+    public Start(ServicePromise servicePromise) {
+        this.servicePromise = servicePromise;
+    }
+
+    private boolean success;
+
+    @Override
+    public Promise generate() {
+        return servicePromise.get(getClass().getSimpleName(), 12_000L)
+                .then("start", (_, _, promise) -> {
+                    TelegramCommandContext context = promise.getRepositoryMapClass(TelegramCommandContext.class);
+                    context.getTelegramBot().send(
+                            context.getIdChat(),
+                            "Привет",
+                            null
+                    );
+                });
+    }
+
+}
