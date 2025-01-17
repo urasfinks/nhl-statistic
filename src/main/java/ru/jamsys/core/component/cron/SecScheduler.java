36081ad7840f2eb2f366a4a16a9455b06ca97080
sfinks <urasfinks@yandex.ru>
2025-01-17 18:40:31 +0300

    .then("bug01", (_, _, _) -> {})

--- src/main/java/ru/jamsys/core/component/cron/SecScheduler.java
+++ src/main/java/ru/jamsys/core/component/cron/SecScheduler.java
@@ -44,7 +44,7 @@ public class SecScheduler implements Cron1s, PromiseGenerator, UniqueClassName {
     public Promise generate() {
         //System.out.println("GEN " + getClassName());
         return servicePromise.get(getClass().getSimpleName(), 600_000L)
-                .then("test", (atomicBoolean, promiseTask, promise) -> {
+                .then("bug01", (atomicBoolean, promiseTask, promise) -> {
                     if (App.get(TelegramBotComponent.class).getBotRepository().size() < 2) {
                         promise.skipAllStep("size bot < 2");
                     }
