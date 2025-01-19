0943aecb17afc5a441e3eb48a636bf0d4916b410
sfinks <urasfinks@yandex.ru>
2025-01-19 20:00:29 +0300

    Тесты рассылки по участникам

--- src/main/java/ru/jamsys/NhlStatisticApplication.java
+++ src/main/java/ru/jamsys/NhlStatisticApplication.java
@@ -1,5 +1,7 @@
 package ru.jamsys;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import ru.jamsys.core.App;
 import ru.jamsys.core.flat.util.Util;
@@ -23,26 +25,43 @@ public class NhlStatisticApplication {
         App.main(args);
     }
 
+    @Getter
+    @Setter
+    public static class MSG{
+        final String data;
+        final Long idChat;
+
+        public MSG(String data, Long idChat) {
+            this.data = data;
+            this.idChat = idChat;
+        }
+    }
+
     @SuppressWarnings("unused")
     public static void loadTelegram(){
-        Queue<String> queue = new ConcurrentLinkedQueue<>();
-        for (int i = 0; i < 50; i++) {
-            queue.add(i + "");
+        Queue<MSG> queue = new ConcurrentLinkedQueue<>();
+        for (int i = 0; i < 30; i++) {
+            queue.add(new MSG(i + "", 290029195L));
+            queue.add(new MSG(i + "", 294097034L));
+            queue.add(new MSG(i + "", 241022301L));
         }
         long start = System.currentTimeMillis();
         AvgMetric avg = new AvgMetric();
         AtomicBoolean a = new AtomicBoolean(false);
+        AtomicBoolean isRun = new AtomicBoolean(true);
         for (int i = 0; i < 1; i++) {
             new Thread(() -> {
-                while (!queue.isEmpty()) {
-                    String poll = queue.poll();
+                while (isRun.get()) {
+                    MSG poll = queue.poll();
                     if (poll != null) {
-                        HttpResponse send = send(poll);
+                        HttpResponse send = send(poll.getIdChat(), poll.getData());
                         avg.add(send.getTiming());
-                        Util.logConsoleJson(send);
+                        //Util.logConsoleJson(send);
+                    }else{
+                        Util.sleepMs(1000);
                     }
                 }
-                Util.logConsole("Timing send: " + (System.currentTimeMillis() - start));
+                //Util.logConsole("Timing send: " + (System.currentTimeMillis() - start));
                 if (a.compareAndSet(false, true)) {
                     Util.sleepMs(5000);
                     Util.logConsoleJson(avg.flushInstance());
@@ -51,12 +70,13 @@ public class NhlStatisticApplication {
         }
     }
 
-    public static HttpResponse send(String data) {
+    public static HttpResponse send(long idChat, String data) {
         HttpClientImpl httpClient = new HttpClientImpl();
         httpClient.setUrl(String.format(
                 "https://api.telegram.org/bot%s/sendMessage?parse_mode=markdown&chat_id=%s&text=%s",
+                //"http://176.124.217.254:8081/bot%s/sendMessage?parse_mode=markdown&chat_id=%s&text=%s",
                 "",
-                290029195L,
+                idChat,
                 URLEncoder.encode(data, StandardCharsets.UTF_8))
         );
         httpClient.setTimeoutMs(10_000);
