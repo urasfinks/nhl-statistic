6cd070a71dd6e53830d4168281543de7cd47bbc1
urasfinks <urasfinks@yandex.ru>
2025-02-15 20:53:22 +0300

    Добавление подписчика

--- src/main/java/ru/jamsys/core/handler/telegram/mother/StartMother.java
+++ src/main/java/ru/jamsys/core/handler/telegram/mother/StartMother.java
@@ -6,10 +6,16 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.jamsys.NhlStatisticApplication;
 import ru.jamsys.core.component.ServicePromise;
+import ru.jamsys.core.jt.JTSubscriber;
 import ru.jamsys.core.promise.Promise;
 import ru.jamsys.core.promise.PromiseGenerator;
+import ru.jamsys.core.resource.jdbc.JdbcRequest;
+import ru.jamsys.core.resource.jdbc.JdbcResource;
 import ru.jamsys.telegram.TelegramCommandContext;
 import ru.jamsys.telegram.handler.MotherBotCommandHandler;
+
+import java.util.List;
+import java.util.Map;
 
 @SuppressWarnings("unused")
 @Setter
@@ -28,6 +34,22 @@ public class StartMother implements PromiseGenerator, MotherBotCommandHandler {
     public Promise generate() {
         return servicePromise.get(getClass().getSimpleName(), 12_000L)
                 .extension(promise -> promise.setRepositoryMapClass(StartMother.class, this))
+                .thenWithResource("subscribe", JdbcResource.class, (_, _, promise, jdbcResource) -> {
+                    TelegramCommandContext context = promise.getRepositoryMapClass(TelegramCommandContext.class);
+                    List<Map<String, Object>> result = jdbcResource.execute(new JdbcRequest(JTSubscriber.SELECT)
+                            .addArg("id_chat", context.getIdChat())
+                            .addArg("bot", context.getTelegramBot().getBotUsername())
+                    );
+                    if (result.isEmpty()) {
+                        promise.setRepositoryMapClass(Boolean.class, true);
+                        jdbcResource.execute(new JdbcRequest(JTSubscriber.INSERT)
+                                .addArg("bot", context.getTelegramBot().getBotUsername())
+                                .addArg("id_chat", context.getIdChat())
+                                .addArg("user_info", context.getUserInfo())
+                                .addArg("playload", context.getUriParameters().getOrDefault("playload", ""))
+                        );
+                    }
+                })
                 .then("start", (_, _, promise) -> {
                     TelegramCommandContext telegramContext = promise.getRepositoryMapClass(TelegramCommandContext.class);
                     telegramContext.getTelegramBot().send(
